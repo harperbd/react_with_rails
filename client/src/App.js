fa8fc8b1ea0febaef2e96431185ae75782764c61
Brad.Harper <Brad.Harper@consensuscorp.com>
2018-12-18 19:23:39 -0700

    some react goodies

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import { Container } from 'semantic-ui-react';
 
 class App extends Component {
+  state = { todos: [] };
+
+  componentDidMount() {
+
+  }
+
+  addItem = (name) => {
+
+  };
+
+  updateTodo = (id) => {
+
+  };
+
+  deleteTodo = (id) => {
+
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <TodoForm addItem={this.addItem} />
+        <br />
+        <br />
+        <TodoList />
+      </Container>
     );
   }
 }
